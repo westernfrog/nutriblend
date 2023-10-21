84ea22e3ea526762205b2b52d0377ea6f69a8fd5
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-10-21 23:15:07 +0530

    Updated

--- app/plan/page.jsx
+++ app/plan/page.jsx
@@ -14,12 +14,12 @@ export default function Plan() {
   return (
     <>
       <div className="bg-[url('https://images.unsplash.com/photo-1517178313064-9447953f46e8')] lg:h-screen lg:w-screen bg-cover bg-center">
-        <div className="lg:p-12 px-6 py-8 backdrop-blur w-full h-full bg-black/30">
-          <div className="flex items-center justify-center gap-10 mb-12">
+        <div className="lg:p-12 px-6 py-8 backdrop-blur-lg w-full h-full bg-black/40">
+          <div className="flex flex-wrap items-center justify-center gap-3 lg:gap-10 mb-12">
             {week.map((item, index) => (
               <button
                 key={index}
-                className={`text-neutral-300 hover:bg-neutral-800 ring-1 ring-neutral-800 py-2 px-5 text-sm rounded-full transition duration-300 ease-in-out ${
+                className={`col-span-6 text-neutral-300 hover:bg-neutral-800 ring-1 ring-neutral-800 py-2 px-5 text-sm rounded-full transition duration-300 ease-in-out ${
                   selectedDay === index ? "bg-neutral-700" : "bg-neutral-900"
                 }`}
                 onClick={() => handleDayClick(index)}
